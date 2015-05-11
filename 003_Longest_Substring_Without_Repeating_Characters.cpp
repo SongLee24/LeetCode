dd889600b0545c0bc106dd3e26f3ac240dcd7002
Song Lee <lisong.shine@qq.com>
2015-05-12 00:40:04 +0800

    update

---
+++ 003_Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+    	int maxlen = 0;
+    	string buffer;
+    	for(int i=0; i<s.size(); ++i)
+    	{
+    		int pos = buffer.find(s[i]);
+    		if(pos >= 0)   // 重复
+    		{
+    		    // 更新最长长度
+    			maxlen = maxlen > buffer.size()? maxlen : buffer.size();
+    			// 删除从0到pos的所有字符
+    			buffer.erase(buffer.begin(), buffer.begin()+pos+1);
+    		}
+    		buffer += s[i];
+    	}
+    	return maxlen > buffer.size() ? maxlen : buffer.size();
+    }
+};
