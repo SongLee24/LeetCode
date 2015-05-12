6896e876a834c53388fb25a31cb5a621c47bac0a
Song Lee <lisong.shine@qq.com>
2015-05-12 12:54:43 +0800

    update

---
+++ 004_Median_of_Two_Sorted_Arrays.cpp
@@ -0,0 +1,38 @@
+﻿class Solution {
+public:
+    int findKth(int A[], int m, int B[], int n, int k)  
+    {  
+    	if (m > n)    // 保证数组A的大小总是小于数组B的大小
+    		return findKth(B, n, A, m, k);  
+    	if (m == 0)   // 如果A为空
+    		return B[k - 1];  
+    	if (k == 1)   // 如果k为1
+    		return min(A[0], B[0]);  
+    	
+    	// 每次把k分两半
+    	int pa = min(k/2, m);  /*A的元素可能小于k/2个*/
+    	int pb = k - pa;
+    
+    	if (A[pa-1] < B[pb-1])  
+    		return findKth(A+pa, m-pa, B, n, k-pa);  
+    
+    	else if (A[pa-1] > B[pb-1])  
+    		return findKth(A, m, B+pb, n-pb, k-pb);  
+    	else  
+    		return A[pa-1];  
+    } 
+    
+     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    	int *A = nums1.data();
+    	int *B = nums2.data();
+    	int m = nums1.size();
+    	int n = nums2.size();
+    
+    	int total = m+n;
+    	if (total%2 == 1)  // 奇数个
+    		return findKth(A, m, B, n, total/2+1);  
+    	else  
+    		return (findKth(A, m, B, n, total/2) 
+    				+ findKth(A, m, B, n, total/2+1)) / 2.0; 
+     }
+};
---
+++ 209_Minimum_Size_Subarray_Sum.cpp
@@ -0,0 +1,23 @@
+﻿class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+    	int len = nums.size();
+    	if(len == 0) return 0; // 数组为空
+    	int minlen = INT_MAX;
+    	int sum = 0;
+    	
+    	int left = 0;          // 滑动窗口的左右边沿
+    	int right = -1;
+    	while(right < len)
+    	{
+    		while(sum < s && right < len)
+    			sum += nums[++right];  // 向右延伸
+    		if(sum >= s)
+    		{
+    			minlen = minlen < right-left+1 ? minlen : right-left+1;
+    			sum -= nums[left++];
+    		}
+    	}
+    	return minlen > len ? 0 : minlen;
+    }
+};
