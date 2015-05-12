d04ead33ba31c4555fe20294d20efe4a7f270f82
Song Lee <lisong.shine@qq.com>
2015-05-12 22:09:56 +0800

    update

---
+++ 007_Reverse_Integer.cpp
@@ -0,0 +1,23 @@
+﻿class Solution {
+public:
+    int reverse(int x) {
+    	uint64_t n = x>0 ? x:-x;
+    	stack<int> s; // 存储每一位
+    	while(n) {
+    		s.push(n%10);
+    		n = n/10;
+    	}
+    
+    	uint64_t m10 = 1;
+    	while(!s.empty()) {
+    		n += m10 * s.top();
+    		s.pop();
+    		m10 *= 10;
+    	}
+    	
+    	if(n > INT_MAX)  // 如果超出范围
+    		return 0;
+    	else
+    		return x>0? n:-n;
+    }
+};
