c65159c5a421655d8c03fd4c9aa047a9db97fabc
Song Lee <lisong.shine@qq.com>
2015-05-13 22:21:36 +0800

    update

---
+++ 008_String_to_Integer_(atoi).cpp
@@ -0,0 +1,38 @@
+﻿class Solution {
+public:
+    int myAtoi(string str) {
+        int i=-1;
+        bool positive = true;    // 标记正负
+        while(str[++i]==' ') ;   // 跳过空字符
+        if(str[i]=='-' || str[i]=='+') {
+            positive = str[i]=='-'?false:true;
+            ++i;
+        }
+
+        if(!isdigit(str[i]))     // 若第一个字符是非数字字符
+            return 0;
+
+        uint64_t sum = 0;
+        uint64_t m10 = 1;
+        for(int j=str.size()-1; j>=i; --j)
+        {
+            if(!isdigit(str[j]))  // 若中间出现非数字字符
+            {
+                sum = 0;
+                m10 = 1;
+                continue;
+            }
+            sum += (str[j]-'0') * m10;
+            m10 *= 10;
+            if(sum > INT_MAX)
+                break;
+        }
+
+        if(sum > INT_MAX && positive)
+            return INT_MAX;
+        else if(sum > INT_MAX && !positive)
+            return INT_MIN;
+        else
+            return positive?sum:-sum;
+    }
+};
