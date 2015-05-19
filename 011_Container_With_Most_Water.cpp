c807449c43e5a7716c9b02b9ca1f103d2022caf6
Song Lee <lisong.shine@qq.com>
2015-05-19 10:06:45 +0800

    update

---
+++ 011_Container_With_Most_Water.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+    int maxArea(vector<int>& height) {
+    	int res = 0;
+    	int i = 0;
+    	int j = height.size()-1;
+    	while(i < j)
+    	{
+    		res = max(res, (j-i)*min(height[i],height[j]));
+    		if(height[i] < height[j])
+    			++i;
+    		else
+    			--j;
+    	}
+    	return res;
+    }
+};
