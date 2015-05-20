d933a2133e08038c224d72cef3ea059c3dfb4f68
Song Lee <lisong.shine@qq.com>
2015-05-20 17:52:19 +0800

    update

---
+++ 020_Valid_Parentheses.cpp
@@ -0,0 +1,19 @@
+﻿class Solution {
+public:
+    bool isValid(string s) {
+    	stack<char> Stack;
+    	for(int i=0; i<s.size(); ++i) {
+    		if(Stack.empty())
+    			Stack.push(s[i]);
+    		else if((Stack.top()=='(' && s[i]==')') || (Stack.top()=='{' && s[i]=='}') || (Stack.top()=='[' && s[i]==']'))
+    			Stack.pop();
+    		else
+    			Stack.push(s[i]);
+    	}
+    
+    	if(Stack.empty())
+    		return true;
+    	else
+    		return false;
+    }
+};
