b84806c5110ea1181d1b0c34a099ae20b310358d
Song Lee <lisong.shine@qq.com>
2015-05-20 13:38:04 +0800

    update

---
+++ 019_Remove_Nth_Node_From_End_of_List.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* p = new ListNode(0);
+        p->next = head;
+        
+        ListNode* slow = p;     // 快慢指针
+        ListNode* fast = p;
+        while(n--)              // fast先行n步
+            fast = fast->next;
+        
+        while(fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        slow->next = slow->next->next;
+        
+        return p->next;
+    }
+};
