babe8c247840303978bdd84a3d20b84302aa4ab7
Song Lee <lisong.shine@qq.com>
2015-05-20 18:13:29 +0800

    update

---
+++ 021_Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,26 @@
+﻿class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode(0);
+        ListNode* p = head;
+        while(l1!=NULL && l2!=NULL) {
+            if(l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                p->next = l2;
+                l2 = l2->next;
+            }
+            p = p->next;
+            p->next = NULL;
+        }
+        
+        if(l1!=NULL)
+            p->next = l1;
+        if(l2!=NULL)
+            p->next = l2;
+        
+        return head->next;
+    }
+};
