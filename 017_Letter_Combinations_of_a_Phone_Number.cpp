ca9e589ce04a1623b8f4032ff0adce24ff72b043
Song Lee <lisong.shine@qq.com>
2015-05-20 11:30:09 +0800

    update

---
+++ 017_Letter_Combinations_of_a_Phone_Number.cpp
@@ -0,0 +1,24 @@
+﻿class Solution {
+    static string letter[10];
+    vector<string> res;
+public:
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty())
+            return res;
+    	string str;
+    	combine(digits, 0, str);
+    	return res;
+    }
+    
+    void combine(string &digits, int i, string str) {
+    	if(i>=digits.size()) {   // 递归终止条件
+    		res.push_back(str);
+    		return;
+    	}
+    	string s = letter[digits[i]-'0'];
+    	for(int j=0; j<s.size(); ++j)
+    		combine(digits, i+1, str+s[j]);		
+    }
+};
+
+string Solution::letter[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
