7f18ba41971759adcf81676710ae62babcae8681
Song Lee <lisong.shine@qq.com>
2015-05-24 10:45:15 +0800

    update

---
+++ 027_Remove_Element.cpp
@@ -0,0 +1,13 @@
+﻿class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int>::iterator beg = nums.begin();
+        for( ; beg!=nums.end(); ) {
+            if(*beg == val)
+                beg = nums.erase(beg);
+            else
+                ++beg;
+        }
+        return nums.size();
+    }
+};
