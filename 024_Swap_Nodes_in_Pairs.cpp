0b5e2677b4b9c4a5754bad1c9cc6a35aaab73acd
Song Lee <lisong.shine@qq.com>
2015-05-24 08:58:48 +0800

    update

---
+++ 024_Swap_Nodes_in_Pairs.cpp
@@ -0,0 +1,27 @@
+﻿class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == NULL)
+            return NULL;
+        ListNode* first = head;
+        ListNode* second = head->next;
+        ListNode* p = new ListNode(0);
+        head = p;
+        while(first!=NULL && second!=NULL) {
+            ListNode* temp1 = first;
+            ListNode* temp2 = second;
+            if(second->next!=NULL)
+                second = second->next->next;
+            if(first->next!=NULL)
+                first = first->next->next;
+            p->next = temp2;
+            p->next->next = temp1;
+            p = p->next->next;
+            p->next = NULL;
+        }
+        if(first!=NULL) {
+            p->next = first;
+        }
+        return head->next;
+    }
+};
